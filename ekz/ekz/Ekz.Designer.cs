904d1db1b3985235e2dee5aa28c048f80911865a
terentev01 <64262621+terentev01@users.noreply.github.com>
2020-04-25 10:07:03 +0300

    Создание главной формы

---
+++ ekz/ekz/Ekz.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ekz
+{
+    partial class Ekz
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.f1txtNaprav = new System.Windows.Forms.TextBox();
+            this.f1txtProdolz = new System.Windows.Forms.TextBox();
+            this.f1txtCena = new System.Windows.Forms.TextBox();
+            this.f1btnSave = new System.Windows.Forms.Button();
+            this.labelnapr = new System.Windows.Forms.Label();
+            this.labelprod = new System.Windows.Forms.Label();
+            this.labelcena = new System.Windows.Forms.Label();
+            this.labelrazm = new System.Windows.Forms.Label();
+            this.f1nudRazm = new System.Windows.Forms.NumericUpDown();
+            this.f1btnOk = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.f1nudRazm)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(685, 282);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // f1txtNaprav
+            // 
+            this.f1txtNaprav.Location = new System.Drawing.Point(597, 304);
+            this.f1txtNaprav.Name = "f1txtNaprav";
+            this.f1txtNaprav.Size = new System.Drawing.Size(100, 20);
+            this.f1txtNaprav.TabIndex = 1;
+            // 
+            // f1txtProdolz
+            // 
+            this.f1txtProdolz.Location = new System.Drawing.Point(597, 330);
+            this.f1txtProdolz.Name = "f1txtProdolz";
+            this.f1txtProdolz.Size = new System.Drawing.Size(100, 20);
+            this.f1txtProdolz.TabIndex = 2;
+            // 
+            // f1txtCena
+            // 
+            this.f1txtCena.Location = new System.Drawing.Point(597, 356);
+            this.f1txtCena.Name = "f1txtCena";
+            this.f1txtCena.Size = new System.Drawing.Size(100, 20);
+            this.f1txtCena.TabIndex = 3;
+            // 
+            // f1btnSave
+            // 
+            this.f1btnSave.Location = new System.Drawing.Point(597, 382);
+            this.f1btnSave.Name = "f1btnSave";
+            this.f1btnSave.Size = new System.Drawing.Size(100, 23);
+            this.f1btnSave.TabIndex = 4;
+            this.f1btnSave.Text = "Сохранить";
+            this.f1btnSave.UseVisualStyleBackColor = true;
+            // 
+            // labelnapr
+            // 
+            this.labelnapr.AutoSize = true;
+            this.labelnapr.Location = new System.Drawing.Point(484, 307);
+            this.labelnapr.Name = "labelnapr";
+            this.labelnapr.Size = new System.Drawing.Size(75, 13);
+            this.labelnapr.TabIndex = 5;
+            this.labelnapr.Text = "Направление";
+            // 
+            // labelprod
+            // 
+            this.labelprod.AutoSize = true;
+            this.labelprod.Location = new System.Drawing.Point(484, 333);
+            this.labelprod.Name = "labelprod";
+            this.labelprod.Size = new System.Drawing.Size(111, 13);
+            this.labelprod.TabIndex = 6;
+            this.labelprod.Text = "Продолжительность";
+            // 
+            // labelcena
+            // 
+            this.labelcena.AutoSize = true;
+            this.labelcena.Location = new System.Drawing.Point(484, 359);
+            this.labelcena.Name = "labelcena";
+            this.labelcena.Size = new System.Drawing.Size(33, 13);
+            this.labelcena.TabIndex = 7;
+            this.labelcena.Text = "Цена";
+            // 
+            // labelrazm
+            // 
+            this.labelrazm.AutoSize = true;
+            this.labelrazm.Location = new System.Drawing.Point(12, 307);
+            this.labelrazm.Name = "labelrazm";
+            this.labelrazm.Size = new System.Drawing.Size(93, 13);
+            this.labelrazm.TabIndex = 8;
+            this.labelrazm.Text = "Размер массива";
+            // 
+            // f1nudRazm
+            // 
+            this.f1nudRazm.Location = new System.Drawing.Point(12, 325);
+            this.f1nudRazm.Name = "f1nudRazm";
+            this.f1nudRazm.Size = new System.Drawing.Size(120, 20);
+            this.f1nudRazm.TabIndex = 9;
+            // 
+            // f1btnOk
+            // 
+            this.f1btnOk.Location = new System.Drawing.Point(12, 352);
+            this.f1btnOk.Name = "f1btnOk";
+            this.f1btnOk.Size = new System.Drawing.Size(75, 23);
+            this.f1btnOk.TabIndex = 10;
+            this.f1btnOk.Text = "OK";
+            this.f1btnOk.UseVisualStyleBackColor = true;
+            this.f1btnOk.Click += new System.EventHandler(this.f1btnOk_Click);
+            // 
+            // Ekz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 409);
+            this.Controls.Add(this.f1btnOk);
+            this.Controls.Add(this.f1nudRazm);
+            this.Controls.Add(this.labelrazm);
+            this.Controls.Add(this.labelcena);
+            this.Controls.Add(this.labelprod);
+            this.Controls.Add(this.labelnapr);
+            this.Controls.Add(this.f1btnSave);
+            this.Controls.Add(this.f1txtCena);
+            this.Controls.Add(this.f1txtProdolz);
+            this.Controls.Add(this.f1txtNaprav);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Ekz";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.f1nudRazm)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox f1txtNaprav;
+        private System.Windows.Forms.TextBox f1txtProdolz;
+        private System.Windows.Forms.TextBox f1txtCena;
+        private System.Windows.Forms.Button f1btnSave;
+        private System.Windows.Forms.Label labelnapr;
+        private System.Windows.Forms.Label labelprod;
+        private System.Windows.Forms.Label labelcena;
+        private System.Windows.Forms.Label labelrazm;
+        private System.Windows.Forms.NumericUpDown f1nudRazm;
+        private System.Windows.Forms.Button f1btnOk;
+    }
+}
+
---
+++ ekz/ekz/Ekz.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ekz
+{
+   
+    public partial class Ekz : Form
+    { 
+        public Ekz()
+        {
+            InitializeComponent();
+
+            labelnapr.Visible = false;
+            labelprod.Visible = false;
+            labelcena.Visible = false;
+            f1txtNaprav.Visible = false;
+            f1txtProdolz.Visible = false;
+            f1txtCena.Visible = false;
+            f1btnSave.Visible = false;
+        }
+
+        private void f1btnOk_Click(object sender, EventArgs e)
+        {
+            labelnapr.Visible = true;
+            labelprod.Visible = true;
+            labelcena.Visible = true;
+            f1txtNaprav.Visible = true;
+            f1txtProdolz.Visible = true;
+            f1txtCena.Visible = true;
+            f1btnSave.Visible = true;
+
+            labelrazm.Visible = false;
+            f1nudRazm.Visible = false;
+            f1btnOk.Visible = false;
+        }
+    }
+}
